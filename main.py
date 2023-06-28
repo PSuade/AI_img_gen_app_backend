dc14fb47497697635602553e65f007290af98186
Matthew Marberry <matt.marberry@contractors.mailchimp.com>
2023-06-28 19:07:55 -0400

    Start of our server

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
+
+@app.route("/sign-up", methods=['GET', 'POST'])
+@cross_origin()
+def sign_up():
+    json = request.json
+    print(json);
+    return "got your info"
+
+        # // we want to do things with the data
+        # Parse the data from the payload
+        # Insert into data base
+        # Return a status code
+    
+    
