12b1d88773e52889b50fc7e6ef88a80d4235a71e
Matthew Marberry <matt.marberry@contractors.mailchimp.com>
2023-08-30 18:33:58 -0400

    Wiring up database to accept sign ups for users via API endpoint.

--- main.py
+++ main.py
@@ -3,32 +3,54 @@ from flask import Flask, render_template, request, url_for, redirect
 from flask_cors import CORS, cross_origin
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import func
+import psycopg2
+from configparser import ConfigParser
 
 app = Flask(__name__)
 CORS(app)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///postgres:1234@localhost:5432/ai_app_db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+def get_db_info(filename,section):
+    parser=ConfigParser()
+    parser.read([filename])
 
-db = SQLAlchemy(app)
+    db_info={}
+    if parser.has_section(section):
+         key_val_tuple = parser.items(section) 
+         for item in key_val_tuple:
+             db_info[item[0]]=item[1]
 
+    return db_info
 
 @app.route('/')
-def testdb():
-    try:
-        db.session.query("1").from_statement("SELECT 1").all()
-        return '<h1>It works.</h1>'
-    except:
-        return '<h1>Something is broken.</h1>'
+def get_index_data():
+    return "We will return something"
 
-@app.route("/sign-up", methods=['POST'])
+@app.route('/sign-up', methods=['POST'])
 @cross_origin()
 def sign_up():
     json = request.json
     print(json);
-    return "got your info"
+
+    try:
+        db_params = get_db_info('database.ini', 'postgresql')
+        conn = psycopg2.connect(**db_params);
+        # create a cursor
+        cur = conn.cursor()
+        cur.execute("INSERT INTO users (id, first_name, last_name, email, password) VALUES (%s, %s, %s, %s, %s)", (2, json["firstName"], json["lastName"], json["email"], json["password"])) 
+        conn.commit()
+        cur.close()
+        conn.close()
+
+        return {
+            "status": 200
+        }
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        return {
+            "status": 500
+        }
 
         # // we want to do things with the data
         # Parse the data from the payload
