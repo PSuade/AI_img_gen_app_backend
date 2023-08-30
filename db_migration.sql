27b76f0938ff5261c72f088c03e29b1d96db984e
Matthew Marberry <matt.marberry@contractors.mailchimp.com>
2023-08-30 17:04:49 -0400

    Modifications to DB file.

--- db_migration.sql
+++ db_migration.sql
@@ -1,9 +1,9 @@
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users(
-   ID INT PRIMARY NOT NULL,
-   first_name TEXT NOT NULL,
-   last_name TEXT NOT NULL,
-   email TEXT UNIQUE NOT NULL,
-   password  TEXT NOT NULL,
+   ID INT PRIMARY KEY NOT NULL,
+   first_name VARCHAR(50) NOT NULL,
+   last_name VARCHAR(50) NOT NULL,
+   email VARCHAR(50) UNIQUE NOT NULL,
+   password  VARCHAR(50) NOT NULL
 );
--- main.py
+++ main.py
@@ -1,13 +1,27 @@
-from flask import Flask, request
+import os
+from flask import Flask, render_template, request, url_for, redirect
 from flask_cors import CORS, cross_origin
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
 
 app = Flask(__name__)
 CORS(app)
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 
-@app.route("/")
-def hello_world():
-    return "Hello, World!"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///postgres:1234@localhost:5432/ai_app_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+@app.route('/')
+def testdb():
+    try:
+        db.session.query("1").from_statement("SELECT 1").all()
+        return '<h1>It works.</h1>'
+    except:
+        return '<h1>Something is broken.</h1>'
 
 @app.route("/sign-up", methods=['POST'])
 @cross_origin()
