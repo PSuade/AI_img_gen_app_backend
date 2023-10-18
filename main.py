016618f02888655d3c27f8adc68927d4c3b3a394
Patrick <pmahon@students.kennesaw.edu>
2023-10-18 18:16:02 -0400

    front end sign up code

--- main.py
+++ main.py
@@ -36,6 +36,34 @@ def sign_up():
             "status": 500
         }
 
+
+@app.route('/log-in', methods=['POST'])
+@cross_origin()
+def log_in():
+    json = request.json
+    print(json);
+
+    try:
+        email = json["email"]
+        password = json["password"]
+
+        user = User.get_user_by_email(email)
+        
+        if(password == user[4]):
+            return {
+                "status": 200
+            }
+        else:
+            return {
+                "status": 400
+            }
+
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        return {
+            "status": 500
+        }        
+
         # // we want to do things with the data
         # Parse the data from the payload
         # Insert into data base
